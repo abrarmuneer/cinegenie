5a5a6c404ece7a7a0b0763557b6edfed9b2b54ae
Abrar <abrarmuneer123@gmail.com>
2026-04-22 18:29:00 +0530

    final railway port fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 import pandas as pd
+import os
 
 app = Flask(__name__)
 app.secret_key = "secret123"
@@ -108,4 +109,4 @@ def logout():
 # Run App
 # ======================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
